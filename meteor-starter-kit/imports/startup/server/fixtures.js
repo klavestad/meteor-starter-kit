6e35c894e2e498ac665ec6dff5ebaa69ffad8d56
Sindre Klavestad <sindreklavestad@Sindres-MacBook-Pro-3.local>
2019-04-03 20:27:23 +0200

    added fixtures, which will update the db if it's empty

--- meteor-starter-kit/imports/startup/server/fixtures.js
+++ meteor-starter-kit/imports/startup/server/fixtures.js
@@ -1,34 +1,51 @@
-// Fill the DB with example data on startup
-
-import { Meteor } from 'meteor/meteor';
-import { Links } from '../../api/links/links.js';
+import {
+  Meteor
+} from 'meteor/meteor';
+import {
+  Patients
+} from '../../api/patients/patients.js';
 
 Meteor.startup(() => {
   // if the Links collection is empty
-  if (Links.find().count() === 0) {
-    const data = [
-      {
-        title: 'Do the Tutorial',
-        url: 'https://www.meteor.com/try',
-        createdAt: new Date(),
+  if (Patients.find().count() === 0) {
+    const data = [{
+        name: 'Sindre Klavestad',
+        age: '79',
+        address: "Christian Kroghs Gate 32, 0186 Oslo",
+        description: "Patient is suffering from insomnia due to working on the master thesis",
+        imgsrc: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+        connection: "ok",
+        health: "ok",
+        lastconnected: new Date(),
+        adl: [],
+        sensors: []
       },
       {
-        title: 'Follow the Guide',
-        url: 'http://guide.meteor.com',
-        createdAt: new Date(),
+        name: 'Ola Nordmann',
+        age: '84',
+        address: "Christian Kroghs Gate 32, 0186 Oslo",
+        description: "Patient is suffering from dementia",
+        imgsrc: "https://semantic-ui.com/images/avatar/large/steve.jpg",
+        connection: "ok",
+        health: "ok",
+        lastconnected: new Date(),
+        adl: [],
+        sensors: []
       },
       {
-        title: 'Read the Docs',
-        url: 'https://docs.meteor.com',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Discussions',
-        url: 'https://forums.meteor.com',
-        createdAt: new Date(),
+        name: 'Kari Nordmann',
+        age: '86',
+        address: "Christian Kroghs Gate 32, 0186 Oslo",
+        description: "Patient has a history of backproblems",
+        imgsrc: "https://semantic-ui.com/images/avatar2/large/molly.png",
+        connection: "ok",
+        health: "ok",
+        lastconnected: new Date(),
+        adl: [],
+        sensors: []
       },
     ];
 
-    data.forEach(link => Links.insert(link));
+    data.forEach(patient => Patients.insert(patient));
   }
 });
